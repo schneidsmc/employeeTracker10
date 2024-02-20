a87dfe35fc078aa07b7648f5fb92ee66b68dd55e
snipes <schneidsmc@gmail.com>
2024-02-19 23:21:19 -0600

    server js started, mysql connection not working..

--- Develop/server.js
+++ Develop/server.js
@@ -1,16 +1,8 @@
 const fs = require('fs')
 const path = require('path')
-const express = require('express')
-const app = express()
-
-const PORT = process.env.PORT || 3000
-
-// Creates id for each
-// const { v4 : uuidv4 } = require('uuid');
+const mysql = require('mysql2')
 
 // Public folder access
-app.use(express.static('public'))
-app.use(express.json())
 
 // PROJECT
 
@@ -19,22 +11,99 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       // MySQL username,
-      user: 'schneidsmc',
+      user: 'root',
       // TODO: Add MySQL password here
-      password: 'mcSnipes!2',
-      database: 'employees_db'
-    },
-    console.log(`Connected to the movies_db database.`)
-  );
+      password: '',
+      database: 'CMS_db'
+    })
+
+    db.connect(err =>{
+      if (err) throw err;
+      console.log(`Connected to the CMS_db database.`);
+      afterConnection()
+    })
   
+    afterConnection = () => {
+      console.log("***********************************")
+      console.log("*                                 *")
+      console.log("*        EMPLOYEE MANAGER         *")
+      console.log("*                                 *")
+      console.log("***********************************")
+      promptUser();
+    };
+  // view all employees
+  // view all departments
+  // view all roles
+  // add a department
+  // add a role
+  // add an employee
+  // update employee role
+  // **update employee manager
+  // **view employees by manager
+  // **delete departments, roles and employees
+  // **View salary budget SUM of Salaries
 
   
-  // Default response for any other request (Not Found)
-  app.use((req, res) => {
-    res.status(404).end();
-  });
-//   Listen.
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
+const promtUser = async () => {
+  try{
+    let answer = await inquirer.prompt({
+      name: 'action',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices:  [
+        'View ALL Employees',
+        'View ALL Departments',
+        'View ALL Roles',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update Employee Role',
+        'Exit'
+      ]
+    });
+    switch (answer.action) {
+      case 'View ALL Employees':
+        employeeView();
+        break;
+
+      case 'View ALL Departments':
+        departmentView();
+        break;
+
+      case 'View ALL Roles':
+        roleView();
+        break;
+
+      case 'Add Department':
+        departmentAdd();
+        break;
+
+      case 'Add Role':
+        roleAdd();
+        break;
+
+      case 'Add Employee':
+        employeeAdd();
+        break;
+
+      case 'Update Employee Role':
+        employeeUpdate();
+        break;
+
+      case 'Exit':
+        connection.end();
+        break;
+    };
+  } catch (err) {
+    console.log(err);
+    promtUser();
+  }
+};
+
+// employeeView();
+// departmentView();
+// roleView();
+// departmentAdd();
+// roleAdd();
+// employeeAdd();
+// employeeUpdate();
