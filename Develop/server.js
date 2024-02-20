31b71143f9be4f2916b53756362a87b945d971ae
snipes <schneidsmc@gmail.com>
2024-02-19 23:51:57 -0600

    mysql connection fixed

--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,12 @@
 const fs = require('fs')
 const path = require('path')
 const mysql = require('mysql2')
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
 
-// Public folder access
-
-// PROJECT
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Connect to database
 const db = mysql.createConnection(
@@ -13,7 +15,7 @@ const db = mysql.createConnection(
       // MySQL username,
       user: 'root',
       // TODO: Add MySQL password here
-      password: '',
+      password: 'Root',
       database: 'CMS_db'
     })
 
@@ -29,7 +31,7 @@ const db = mysql.createConnection(
       console.log("*        EMPLOYEE MANAGER         *")
       console.log("*                                 *")
       console.log("***********************************")
-      promptUser();
+      // promptUser();
     };
   // view all employees
   // view all departments
@@ -44,61 +46,61 @@ const db = mysql.createConnection(
   // **View salary budget SUM of Salaries
 
   
-const promtUser = async () => {
-  try{
-    let answer = await inquirer.prompt({
-      name: 'action',
-      type: 'list',
-      message: 'What would you like to do?',
-      choices:  [
-        'View ALL Employees',
-        'View ALL Departments',
-        'View ALL Roles',
-        'Add Department',
-        'Add Role',
-        'Add Employee',
-        'Update Employee Role',
-        'Exit'
-      ]
-    });
-    switch (answer.action) {
-      case 'View ALL Employees':
-        employeeView();
-        break;
+// const promptUser = async () => {
+//   try{
+//     let answer = await inquirer.prompt({
+//       name: 'action',
+//       type: 'list',
+//       message: 'What would you like to do?',
+//       choices:  [
+//         'View ALL Employees',
+//         'View ALL Departments',
+//         'View ALL Roles',
+//         'Add Department',
+//         'Add Role',
+//         'Add Employee',
+//         'Update Employee Role',
+//         'Exit'
+//       ]
+//     });
+//     switch (answer.action) {
+//       case 'View ALL Employees':
+//         employeeView();
+//         break;
 
-      case 'View ALL Departments':
-        departmentView();
-        break;
+//       case 'View ALL Departments':
+//         departmentView();
+//         break;
 
-      case 'View ALL Roles':
-        roleView();
-        break;
+//       case 'View ALL Roles':
+//         roleView();
+//         break;
 
-      case 'Add Department':
-        departmentAdd();
-        break;
+//       case 'Add Department':
+//         departmentAdd();
+//         break;
 
-      case 'Add Role':
-        roleAdd();
-        break;
+//       case 'Add Role':
+//         roleAdd();
+//         break;
 
-      case 'Add Employee':
-        employeeAdd();
-        break;
+//       case 'Add Employee':
+//         employeeAdd();
+//         break;
 
-      case 'Update Employee Role':
-        employeeUpdate();
-        break;
+//       case 'Update Employee Role':
+//         employeeUpdate();
+//         break;
 
-      case 'Exit':
-        connection.end();
-        break;
-    };
-  } catch (err) {
-    console.log(err);
-    promtUser();
-  }
-};
+//       case 'Exit':
+//         connection.end();
+//         break;
+//     };
+//   } catch (err) {
+//     console.log(err);
+//     promptUser();
+//   }
+// };
 
 // employeeView();
 // departmentView();
