39df53db9e7b2d8f74b6e17123b040ea63b8093f
snipes <schneidsmc@gmail.com>
2024-02-19 22:29:34 -0600

    schema & seeds created

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -1,17 +1,25 @@
--- create database
 
+DROP DATABASE IF EXISTS CMS_db;
+CREATE DATABASE CMS_db;
 
---Create the following tables
+USE CMS_db;
 
---Department
-    --id = name
+CREATE TABLE department (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  department_name VARCHAR(30) NOT NULL
+);
 
---Role
-    -- ids = role, salary, 
-    -- foreign key = department id
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary decimal NOT NULL,
+  department_id INT
+);
 
---Employee
-    -- ids = first_name, last_name
-    --foreign key = role id
-    --foreign key = manager id?
-
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT
+);
--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,7 +1,26 @@
---INSERT INTO
 
--- department table (name)
 
--- role table (title, salary)
 
---employee table (first_name, last_name)
+INSERT INTO department(department_name)
+VALUES ("Parks and Recreation"),
+        ("Animal Control"),
+        ("Building Services");
+
+INSERT INTO roles(title, salary, department_id)
+VALUES ("Deparment Manager", 100000, 1),
+        ("Deputy Parks Director", 90000, 1),
+        ("Office Manager", 80000, 1),
+        ("Parks Specialist", 70000, 1),
+        ("Deputy Animal Control Director", 60000, 2),
+        ("Shoe-shiner", 50000, 3);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ron", "Swanson", 1, NULL),
+        ("Leslie", "Knope", 2, 1),
+        ("Donna", "Meagle", 3, 1),
+        ("Tom", "Haverford", 4, 2),
+        ("April", "Ludgate", 5, 2),
+        ("Andy", "Dwyer", 6, 2);
+
+
